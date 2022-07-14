d416ecddaf96685cc2450bddf32b21893748d4e0
drsuneamer <sunyeong0412@gmail.com>
2022-07-14 14:45:15 +0900

    TIL-220713

---
+++ React/[인프런] 처음 만난 리액트/#2. [실습] 직접 리액트 연동하기/MyButton.js
@@ -0,0 +1,13 @@
+function MyButton(props) {
+  const [isClicked, setIsClicked] = React.useState(false);
+
+  return React.createElement(
+    'button',
+    { onClick: () => setIsClicked(true) },
+    isClicked ? 'Clicked!' : 'Click here!'
+  )
+}
+
+// React DOM의 render 함수를 사용하여 리액트 컴포넌트를 DOM 컨테이너에 렌더링하는 코드
+const domContainer = document.querySelector('#root');
+ReactDOM.render(React.createElement(MyButton), domContainer);
