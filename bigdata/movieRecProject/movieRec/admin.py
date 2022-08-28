2e3e0b8424f6d79de30b39368304f63f5ecf7bb1
drsuneamer <sunyeong0412@gmail.com>
2022-08-28 18:58:46 +0900

    220828

--- bigdata/movieRecProject/movieRec/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import User, Movie, Viewed, Recomm
-
-admin.site.register(User)
-admin.site.register(Movie)
-admin.site.register(Viewed)
-admin.site.register(Recomm)
