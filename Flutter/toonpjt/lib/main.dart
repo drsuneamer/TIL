42bf91b16377d19c0883e074f27835b431101fa1
drsuneamer <sunyeong0412@gmail.com>
2024-08-22 00:35:43 +0900

    240822 Flutter #3.6 Cards

--- Flutter/toonpjt/lib/main.dart
+++ Flutter/toonpjt/lib/main.dart
@@ -103,6 +103,77 @@ class App extends StatelessWidget {
                   textColor: Colors.white,
                 ),
               ],
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Wallets",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                  ),
+                ),
+                Text(
+                  "View All",
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.7),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFF1F2123),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(25),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Euro",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            const Text(
+                              "1 440",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Text("EUR",
+                                style: TextStyle(
+                                  color: Colors.white.withOpacity(0.7),
+                                  fontSize: 20,
+                                ))
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ),
