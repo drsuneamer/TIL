7ab3b847b3da21c0cb1fdeb82ff2d967f78897fd
drsuneamer <sunyeong0412@gmail.com>
2024-08-21 23:18:25 +0900

    240821 Flutter #3.2 Buttons Section

--- Flutter/toonpjt/README.md
+++ Flutter/toonpjt/README.md
@@ -6,10 +6,14 @@ A new Flutter project.
 
 Text에 서식 넣고 싶으면 Text("text..", style: TextStyle(color: Colors.white))
 
-Column의 MainAxis는 수직 방향, CrossAxis는 수평 방향
-Row는 MainAxis가 수평, CrossAxis가 수직 방향
+Column(세로 중심)의 MainAxis는 수직 방향, CrossAxis는 수평 방향
+Row(가로 중심)는 MainAxis가 수평, CrossAxis가 수직 방향
+
+space 주려면 SizedBox 사용
 
 색 커스텀 하는 방법
 
 - backgroundColor: Color(0xFF181818),
 - backgroundColor: Color.fromARGB(255, 60, 9, 9)
+
+Container: div와 같이 child를 가지고 있는 단순한 box
--- Flutter/toonpjt/lib/main.dart
+++ Flutter/toonpjt/lib/main.dart
@@ -27,38 +27,89 @@ class App extends StatelessWidget {
       backgroundColor: Color(0xFF181818),
       // column: 수직, row: 수평 배열
       body: Padding(
-        padding: EdgeInsets.all(40),
-        child: Column(children: [
-          SizedBox(
-            // 공간 만들기
-            height: 80,
-          ),
-          Row(
-            // MainAxisAlignment: 수평 정렬
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    'Hello, drsuneamer',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600),
+        padding: EdgeInsets.symmetric(
+          horizontal: 40,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              // 공간 만들기
+              height: 80,
+            ),
+            Row(
+              // MainAxisAlignment: 수평 정렬
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hello, drsuneamer',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.7),
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total balance',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white.withOpacity(0.8),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              '\$ 7 997 412',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF2B33A),
+                    borderRadius: BorderRadius.circular(45),
                   ),
-                  Text(
-                    'Welcome back',
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(0.7),
-                      fontSize: 18,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 15,
+                    ),
+                    child: Text(
+                      'Transfer',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
                   ),
-                ],
-              )
-            ],
-          )
-        ]),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     ));
   }
